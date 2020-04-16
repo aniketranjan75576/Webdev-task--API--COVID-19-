e485cab53078421742dd05408faf6dbbe21839b2
aniketranjan75576 <53879451+aniketranjan75576@users.noreply.github.com>
2020-04-16 22:37:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import Covid19 from './Covid19'
+function App() {
+    return (
+        <div className='App'>
+            <Covid19 />
+        </div>
+    )
+}
+export default App
